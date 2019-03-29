2697849228927e9023e94acd7bb5b31c5c9831b5
captain-c00keys <gpieterson.wsu@hotmail.com>
2019-03-28 18:17:43 -0700

    finished movie db app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MovieRow from './MovieRow.js';
+import $ from 'jquery'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+    // console.log("This is my intializer")
+
+    // const movies = [
+    //   {id: 0, poster_src: "", title: "Avengers: Infinity War", overview: "Definitely a great one"},
+    //   {id: 1, poster_src: "", title: "Avengers", overview: "A great start"},
+    //   {id: 2, poster_src: "", title: "Avengers", overview: "A great start"},
+    //   {id: 3, poster_src: "", title: "Avengers", overview: "A great start"}
+    // ]
+
+    // var movieRows = []
+    // movies.forEach((movie) => {
+    //   console.log(movie.title)
+    //   const movieRow = <MovieRow movie={movie} />
+    //   movieRows.push(movieRow)
+    // })
+
+    // this.state = {rows: movieRows}
+
+    this.performSearch()
+  }
+
+  performSearch(searchTerm) {
+    console.log("Perform search using moviedb")
+    const urlString = "https://api.themoviedb.org/3/search/movie?query=avengers&api_key=227545596a4e028dcff71fa62a94fd9b&language=en-US&page=1&include_adult=false"
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log("Fetched data successfully")
+        // console.log(serchResults)
+        const results = searchResults.results
+        // console.log(results[0])
+
+        var movieRows = []
+
+        results.forEach((movie) => {
+          movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+          // console.log(movie.poster_path)
+          const movieRow = <MovieRow key={movie.id} movie={movie} />
+          movieRows.push(movieRow)
+        })
+
+        this.setState({rows: movieRows})
+      },
+      error: (xhr, status, err) => {
+        console.error("Failed to fetch data")
+      }
+    })
+  }
+
+  searchChangeHandler(event) {
+    console.log(event.target.value)
+    const boundObject = this
+    const searchTerm = event.target.value
+    boundObject.performSearch(searchTerm)
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <table className="titleBar">
+          <tbody>
+            <tr>
+              <td>
+                <img alt="app icon" width="50" src="movie-icon.png"/>
+              </td>
+              <td width="8" />
+              <td>
+                <h1>MoviesDB Search</h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <input style={{
+          fontSize: 24,
+          display: 'block',
+          width: '99%',
+          paddingTop: 8,
+          paddingBottom: 8,
+          paddingLeft: 16
+        }} onChange={this.searchChangeHandler.bind(this)} placeholder="Enter Search Term" />
+
+        {this.state.rows}
+
       </div>
     );
   }
